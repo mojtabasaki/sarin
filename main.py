f0293aeebc482f7df4f357c4fae97970395b7927
ehsan-sh <e.shirinzadeh78@gmail.com>
2024-10-15 15:24:53 +0330

    entries and their variables were created

--- main.py
+++ main.py
@@ -3,5 +3,34 @@ from tkinter import *
 
 screen = Tk()
 
+screen.geometry("800x500+400+200")
+screen.title("CRUD")
+screen.configure(bg="#34b4eb")
+
+
+#variables
+
+Name = StringVar()
+LasName = StringVar()
+StdField = StringVar()
+Age = StringVar()
+
+
+
+
+#entries
+
+txtname = Entry(screen, bd=4,textvariable=Name,width="18", font=("arial 13 bold"), justify="center")
+txtname.place(x=150, y=100)
+
+txtlastname = Entry(screen, bd=4,textvariable=LasName,width="18", font=("arial 13 bold"), justify="center")
+txtlastname.place(x=150, y=150)
+
+txtfield = Entry(screen, bd=4,textvariable=StdField,width="18", font=("arial 13 bold"), justify="center")
+txtfield.place(x=150, y=200)
+
+txtage = Entry(screen, bd=4,textvariable=Age,width="18", font=("arial 13 bold"), justify="center")
+txtage.place(x=150, y=250)
+
 
 screen.mainloop()
